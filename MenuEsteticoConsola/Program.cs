fece1578ac6ec6fc75948d589fc18be4652b2ce7
Bernardo Andres Gonzalez Erramuspe <bgonzalezerramuspe2006@hotmail.com>
2024-04-27 07:05:17 -0300

    Menu Finalizado
    Añadido:
    - el retorno de datos
    - optimizado todo en una funcion

--- MenuEsteticoConsola/Program.cs
+++ MenuEsteticoConsola/Program.cs
@@ -16,72 +16,67 @@ namespace MenuEsteticoConsola
             "Apartentemente no funciona"
         };
 
-        // Posicion del Cursor en la Consola
-        private static int x;
-        private static int y;
-
         static void Main(string[] args)
         {
-            MostrarMenu(OptionMenu);
+            int r = MostrarMenu(OptionMenu);
+            Console.Clear();
+            Console.WriteLine("Opcion Seleccionada: Nº" + r);
+            Console.ReadKey();
         }
 
-        static void MostrarMenu(string[] Opciones) {
+        static int MostrarMenu(string[] Opciones)
+        {
             bool loop = true;
             int counter = 0;
             ConsoleKeyInfo Tecla;
 
             Console.CursorVisible = false; // Con esto hacemos que el cursor no se muestre en consola
 
-            
-
-            while (loop) {
+            while (loop)
+            {
                 Console.Clear();
-                x = Console.CursorLeft;
-                y = Console.CursorTop;
+                Console.WriteLine("Seleccione una opción del menu:" + System.Environment.NewLine);
+                string SeleccionActual = string.Empty;
+                int Destacado = 0;
 
-                string Resultado = LogicaMenu(Opciones, counter);
-                while ((Tecla = Console.ReadKey(true)).Key != ConsoleKey.Enter) { 
-                    switch (Tecla.Key){
-                        case ConsoleKey.DownArrow:
-                            if (counter == Opciones.Length - 1) break;
-                            counter++;
-                            break;
-                        case ConsoleKey.UpArrow:
-                            if (counter == 0) break;
-                            counter--;
-                            break;
-                        default:
-                            break;
+                Array.ForEach(Opciones, element =>
+                {
+                    if (Destacado == counter)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine(" > " + element + " < ");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        SeleccionActual = element;
                     }
-                    Resultado = LogicaMenu(Opciones, counter);
+                    else
+                    {
+                        Console.WriteLine(element);
+                    }
+                    Destacado++;
+                });
+
+                Tecla = Console.ReadKey(true);
+                if (Tecla.Key == ConsoleKey.Enter)
+                {
+                    loop = false;
+                    break;
+                }
+                switch (Tecla.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        if (counter < Opciones.Length - 1) counter++;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (counter > 0) counter--;
+                        break;
+                    default:
+                        break;
                 }
             }
-        }
-
-        private static string LogicaMenu(string[] items, int opcion) {
-            Console.Clear();
-            Console.WriteLine("Seleccione una opción del menu:" + System.Environment.NewLine);
-            string SeleccionActual = string.Empty;
-            int Destacado = 0;
-
-            Array.ForEach(items, element =>
-            {
-                if (Destacado == opcion)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.WriteLine(" > " + element + " < ");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    Console.BackgroundColor = ConsoleColor.Black;
-                    SeleccionActual = element;
-                }
-                else {
-                    Console.WriteLine(element);
-                }
-                Destacado++;
-            });
-
-            return SeleccionActual;
+            return counter;
         }
     }
 }
+
